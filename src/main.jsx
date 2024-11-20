23d72596aaa3977cc827729614a6297f965bff1b
JuliaAris <julia.aris@yandex.ru>
2024-11-20 09:45:23 +0400

    some commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import "/src/Components/HeroCard/HeroCard.css";
 import "/src/Components/HeroCard/HeroCard.jsx";
 import App from "./App.jsx";
 
-ReactDom.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
